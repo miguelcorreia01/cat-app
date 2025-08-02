90b69eb0fec21570bdc1b9bd9d0d5ee397b2c630
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-08-02 15:59:42 +0100

    feat(data): add getImageById method to CatApiService

--- app/src/main/java/com/example/catapp/data/api/CatApiService.kt
+++ app/src/main/java/com/example/catapp/data/api/CatApiService.kt
@@ -1,9 +1,14 @@
 package com.example.catapp.data.api
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface CatApiService {
     @GET("breeds")
     suspend fun getCatBreeds(): List<CatBreedDto>
+
+    @GET("images/{ImageId}")
+    suspend fun getImageById(@Path("imageId") imageId: String): ImageDto
+
 }
