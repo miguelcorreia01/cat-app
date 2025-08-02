8bca4a0269eb563f0f38a16d4150ae53db341680
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-08-02 15:47:34 +0100

    fix(data): Make Dao methods suspendable

--- app/src/main/java/com/example/catapp/data/local/CatBreedDao.kt
+++ app/src/main/java/com/example/catapp/data/local/CatBreedDao.kt
@@ -12,25 +12,25 @@ import kotlinx.coroutines.flow.Flow
 interface CatBreedDao {
 
     @Query("SELECT * FROM cat_breeds")
-    fun getAllCatBreeds(): List<CatBreedEntity>
+    suspend fun getAllCatBreeds(): List<CatBreedEntity>
 
     @Query("SELECT * FROM cat_breeds WHERE id = :id")
-    fun getCatBreedById(id: String): CatBreedEntity
+    suspend fun getCatBreedById(id: String): CatBreedEntity
 
     @Query("SELECT * FROM cat_breeds WHERE isFavorite = 1")
-    fun getFavoriteCatBreeds(): List<CatBreedEntity>
+    suspend fun getFavoriteCatBreeds(): List<CatBreedEntity>
 
     @Query("SELECT * FROM cat_breeds WHERE name LIKE '%' || :searchQuery || '%'")
-    fun searchCatBreeds(searchQuery: String): List<CatBreedEntity>
+    suspend fun searchCatBreeds(searchQuery: String): List<CatBreedEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertCatBreeds(catBreeds: List<CatBreedEntity>)
+    suspend fun insertCatBreeds(catBreeds: List<CatBreedEntity>)
 
     @Update
-    fun updateCatBreed(catBreed: CatBreedEntity)
+    suspend fun updateCatBreed(catBreed: CatBreedEntity)
 
     @Delete
-    fun deleteCatBreed(catBreed: CatBreedEntity)
+    suspend fun deleteCatBreed(catBreed: CatBreedEntity)
 
     @Query("SELECT * FROM cat_breeds")
     fun getAllCatBreedsFlow(): Flow<List<CatBreedEntity>>
