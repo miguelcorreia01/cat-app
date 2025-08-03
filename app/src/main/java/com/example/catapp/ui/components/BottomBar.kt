96a800864cfe053cfaf6f0bdbd04794559b47297
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-08-03 15:59:01 +0100

    feat(ui): add BottomBar

---
+++ app/src/main/java/com/example/catapp/ui/components/BottomBar.kt
@@ -0,0 +1,49 @@
+package com.example.catapp.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Pets
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun BottomBar(selectedTab: String, onTabSelected: (String) -> Unit) {
+
+    NavigationBar(
+        containerColor = Color.White,
+    ) {
+        NavigationBarItem(
+            selected = selectedTab == "Breeds",
+            onClick = { onTabSelected("Breeds") },
+            icon = { Icon(Icons.Filled.Pets, contentDescription = "Breeds") },
+            label = { Text("Breeds") } ,
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = Color.DarkGray,
+                unselectedIconColor = Color.LightGray,
+                selectedTextColor = Color.DarkGray,
+                unselectedTextColor = Color.LightGray,
+                indicatorColor = Color.Transparent
+            )
+        )
+        NavigationBarItem(
+            selected = selectedTab == "Favorites",
+            onClick = { onTabSelected("Favorites") },
+            icon = { Icon(Icons.Filled.FavoriteBorder, contentDescription = "Favorites") },
+            label = { Text("Favorites") } ,
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = Color.DarkGray,
+                unselectedIconColor = Color.LightGray,
+                selectedTextColor = Color.DarkGray,
+                unselectedTextColor = Color.LightGray,
+                indicatorColor = Color.Transparent
+            )
+        )
+
+    }
+
+}
